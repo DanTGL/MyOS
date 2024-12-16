d38140c5ae43ade4525a44f1103e39ee781c9c23
Daniel Hajjar <daniel.hajjar16@gmail.com>
2024-12-16 13:57:25 +0100

    Move from grub to limine

--- iso.sh
+++ iso.sh
@@ -7,9 +7,29 @@ mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
 cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
-cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
-}
-EOF
-grub-mkrescue -o myos.iso isodir
+#cat > isodir/boot/grub/grub.cfg << EOF
+#menuentry "myos" {
+#	multiboot /boot/myos.kernel
+#}
+#EOF
+#grub-mkrescue -o myos.iso isodir
+
+if [ ! -f limine/limine ]; then
+	git clone https://github.com/limine-bootloader/limine.git --branch=v8.x-binary --depth=1
+  	make -C limine
+fi
+
+mkdir -p isodir/boot
+#cp -v kernel/bin/kernel isodir/boot/
+mkdir -p isodir/boot/limine
+cp -v limine.conf limine/limine-bios.sys limine/limine-bios-cd.bin limine/limine-uefi-cd.bin isodir/boot/limine/
+mkdir -p isodir/EFI/BOOT
+cp -v limine/BOOTX64.EFI isodir/EFI/BOOT/
+cp -v limine/BOOTIA32.EFI isodir/EFI/BOOT/
+
+# Create the bootable ISO.
+xorriso -as mkisofs -R -r -J -b boot/limine/limine-bios-cd.bin \
+        -no-emul-boot -boot-load-size 4 -boot-info-table -hfsplus \
+        -apm-block-size 2048 --efi-boot boot/limine/limine-uefi-cd.bin \
+        -efi-boot-part --efi-boot-image --protective-msdos-label \
+        isodir -o myos.iso
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,12 +1,65 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
+#include <stdbool.h>
 
-#include <kernel/tty.h>
 #include <kernel/system.h>
+#include <limine.h>
 
-void kernel_main(void) {
-	terminal_initialize();
-	system_setup();
-	printf("Hello, kernel World!\n");
+// Set the base revision to 3, this is recommended as this is the latest
+// base revision described by the Limine boot protocol specification.
+// See specification for further info.
+
+__attribute__((used, section(".limine_requests")))
+static volatile LIMINE_BASE_REVISION(3);
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent, _and_ they should be accessed at least
+// once or marked as used with the "used" attribute as done here.
+
+__attribute__((used, section(".limine_requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+// Finally, define the start and end markers for the Limine requests.
+// These can also be moved anywhere, to any .c file, as seen fit.
+
+__attribute__((used, section(".limine_requests_start")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end")))
+static volatile LIMINE_REQUESTS_END_MARKER;
+
+static void hcf(void) {
+	for (;;) {
+		asm volatile ("hlt" ::: "memory");
+	}
+}
+
+void kmain(void) {
+	    // Ensure the bootloader actually understands our base revision (see spec).
+    if (LIMINE_BASE_REVISION_SUPPORTED == false) {
+        hcf();
+    }
+
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response == NULL
+     || framebuffer_request.response->framebuffer_count < 1) {
+        hcf();
+    }
+
+    // Fetch the first framebuffer.
+    struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
+
+    // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    for (size_t i = 0; i < 100; i++) {
+        volatile uint32_t *fb_ptr = framebuffer->address;
+        fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+    }
+
+	hcf();
 }
