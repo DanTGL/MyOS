dae21a4a6d8f00760e21ecd0b9752df3c9d38c58
Daniel Hajjar <daniel.hajjar16@gmail.com>
2026-03-10 11:14:22 +0100

    Simplify x86_64 memcpy implementation

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,19 +1,21 @@
 #include <string.h>
 
-#ifdef ARCH_MEMCPY
-void *__arch_memcpy(void *restrict, const void *restrict, size_t);
-#endif /* ifdef ARCH_MEMCPY */
-
 void *memcpy(void *restrict dstptr, const void *restrict srcptr, size_t size)
 {
-#ifdef ARCH_MEMCPY
-    return __arch_memcpy(dstptr, srcptr, size);
-#else
+
     unsigned char *dst       = (unsigned char *)dstptr;
     const unsigned char *src = (const unsigned char *)srcptr;
+
+#ifdef __x86_64__
+    asm volatile("cld; rep movsb"
+                 : "=D"(dst), "=S"(src), "=c"(size)
+                 : "0"(dst), "1"(src), "2"(size)
+                 : "memory");
+#else
     for (size_t i = 0; i < size; i++)
         dst[i] = src[i];
-    return dstptr;
 
-#endif /* ifdef ARCH_MEMCPY */
+#endif /* ifdef __x86_64__ */
+
+    return dstptr;
 }
