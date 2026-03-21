84f759fb1d01144835274cc83a08a2dad09e9fdd
Daniel Hajjar <daniel.hajjar16@gmail.com>
2026-03-21 19:21:09 +0100

    Add helper macros for bit operations

--- kernel/include/utils/helper_macros.h
+++ kernel/include/utils/helper_macros.h
@@ -3,4 +3,13 @@
 
 #define UNUSED(x) ((void)(x))
 
+#define BIT      (1UL << (n))
+#define BIT64(n) (1ULL << (n))
+
+#define BIT_TYPED(n, type) (((type)1U) << (n))
+
+#define BIT_SET(x, n)   ((x) | BIT_TYPED(n, typeof(x)))
+#define BIT_CLEAR(x, n) ((x) & (~BIT_TYPED(n, typeof(x))))
+#define BIT_TOGGLE(x, n) ((x) ^ (BIT_TYPED(n, typeof(x)))
+
 #endif // !MYOS_UTILS_HELPER_MACROS_H
